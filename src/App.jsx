3ef48c19ef4bd73dff9f172a378ec52bfd470338
Katya Weicht <katya.weicht@gmail.com>
2025-06-06 11:02:59 -0600

    Create Projects array

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+const projects = [
+  {
+    name: "Battleship Game", 
+    description: "An interactive battleship game that allows the user to place two ships on a board and take turns with the computer firing upon the other player's board",
+    tools: ["Ruby", "Rspec"],
+    link:"https://github.com/KatyaW-01/Battleship_Project" 
+  },
+  {
+    name: "Futbol Statistics Tracker",
+    description: "This project analyzes team performance in a fictional soccer league across seasons, identifying the best and worst performers and pulling statistics for individual teams",
+    tools: ["Ruby", "Rspec"],
+    link: "https://github.com/JustJoeYo/futbol"
+  },
+  {
+    name: "Vocab Vault Dictionary Api Search",
+    description: "A single page application allowing users to search for a word and receive the pronunciation, part of speach, definition, and examples of the specified word",
+    tools: ["Javascript", "HTML", "CSS"],
+    link: "https://github.com/KatyaW-01/wordly-dictionary-SPA"
+  }
+]
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
   )
 }
 
