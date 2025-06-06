91d4e313faf3b00c7904ad1657d116db1f7082c1
Katya Weicht <katya.weicht@gmail.com>
2025-06-06 16:20:24 -0600

    Add css modules and style project divs

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,9 @@
 import React from "react"
+import styles from '../styles/ProjectCard.module.css'
 
 const ProjectCard = ({project}) => {
   return (
-    <div>
+    <div className={styles.card} >
       <h1>{project.name}</h1>
       <h3>{project.description}</h3>
       <p>Tools: {project.tools}</p>
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import ProjectCard from './ProjectCard'
+import styles from '../styles/ProjectList.module.css'
 
 const ProjectList = ({projects, newProjects = [] }) => {
   if(projects.length === 0 && newProjects.length === 0) {
     return <p>No projects. Please add a new project</p>
   }
   return (
-    <div>
+    <div className={styles.container} >
       {projects.map((project)=>(
         <ProjectCard key={project.id} project={project}/>
       ))}
